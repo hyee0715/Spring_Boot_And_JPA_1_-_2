497419f7584d39670257564605ccbae75561a342
hyee0715 <hyee0715@naver.com>
2021-12-26 23:45:38 +0900

    주문 도메인 개발

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -14,7 +14,7 @@ public class OrderRepository {
 
     private final EntityManager em;
 
-    private void save(Order order) {
+    public void save(Order order) {
         em.persist(order);
     }
 
